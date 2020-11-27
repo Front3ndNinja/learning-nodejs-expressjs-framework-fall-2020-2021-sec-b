c6a250d94ab2598c4fb2134a4f08ff147dd4bfb1
Fardin Ahmed Niloy <niloy98.it@gmail.com>
2020-11-27 23:58:55 +0600

    added feeback by id

--- assignment (food blog)/models/feedback.js
+++ assignment (food blog)/models/feedback.js
@@ -1,18 +1,7 @@
 const db = require('./db');
 
 module.exports = {
-	
-	getById: function (Sid, callback) {
 
-		var sql = "select * from comment where cid='" + Sid + "'";
-		db.getResults(sql, function (results) {
-			//console.log(results.username);
-			callback(results);
-		});
-
-
-
-	},
 	getAll: function (callback) {
 		//uname = 'neil';
 		var sql = "select * from comment";
@@ -20,14 +9,13 @@ module.exports = {
 			callback(results);
 		});
 	},
-	
 
 	update: function (i, cResponse, callback) {
 
 		console.log(cResponse);
 
-        var sql = "UPDATE comment SET cresponse='" + cResponse + "' where cid='" + i + "'";
-        console.log(sql);
+		var sql = "UPDATE comment SET cresponse='" + cResponse + "' where cid='" + i + "'";
+		console.log(sql);
 		db.execute(sql, function (results) {
 			if (results.length > 0) {
 				callback(true);
@@ -38,6 +26,18 @@ module.exports = {
 		});
 
 	},
+
+	getById: function (Sid, callback) {
+
+		var sql = "select * from comment where cid='" + Sid + "'";
+		db.getResults(sql, function (results) {
+			callback(results);
+		});
+
+
+
+	},
+
 	delete: function (Eid, callback) {
 
 		var sql = "DELETE FROM comment WHERE cid='" + Eid + "'";
